0134ad09895f00f5172d129eae93dfe8058e684b
Arun <arunjoset@gmail.com>
2017-10-09 12:28:53 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# kidstime_v1
---
+++ app/src/main/java/com/kidstime/com/kidstime_v1/Splash.java
@@ -0,0 +1,26 @@
+package com.kidstime.com.kidstime_v1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+                    public void run(){
+                         Intent homeIntent = new Intent(Splash.this,MainActivity.class);
+                         startActivity(homeIntent);
+                         finish();
+            }
+
+
+        },SPLASH_TIME_OUT);
+    }
+}
